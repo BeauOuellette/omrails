b6878fdd6c2f1bcf90515837de4e9665d734471a
Beau Ouellette <beau@wreellife.com>
2013-03-19 12:25:00 -0400

    Add User Model with Devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Omrails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Required for devise on heroku
+    config.assets.initialize_on_precompile = false
+    
   end
 end
