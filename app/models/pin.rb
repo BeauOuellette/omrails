bd11a0b59f644f700298aeab0fac0dbecb7496fe
Beau Ouellette <beau@wreellife.com>
2013-03-19 21:05:06 -0400

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,8 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description
 
-  validates :description, presence: :true
+  validates :description, presence: true
+
+  belongs_to :user
+  validates :user_id, presence: true 
 end
